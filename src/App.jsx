70fdc06443ec18d8b31184ca60795f7372486b24
Abdul Rozzaq <abdrozzaq02@gmail.com>
2025-06-28 23:13:15 +0700

    ui: navbar to app main

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Navbar from './components/Navbar'
+
+const App = () => {
+  return (
+    <main className="overflow-x-hidden tracking-tighter bg-black text-gray-200 antialiased">
+      <Navbar/>
+    </main>
+  )
+}
+
+export default App
